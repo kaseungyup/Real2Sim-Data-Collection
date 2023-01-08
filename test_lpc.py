ef2a01342b5a8e9ce273d280f36229e1b9df95bb
sy <timka0506@gmail.com>
2023-01-08 20:39:42 +0900

    test updated

--- test_lpc.py
+++ test_lpc.py
@@ -2,7 +2,6 @@ import rospy
 import numpy as np
 
 from classes.timer import Timer
-from publisher import apriltag_publisher, rpy_publisher
 from subscriber import FlagData, SimulationData
 
 rospy.init_node('lpc', anonymous=True)
@@ -12,17 +11,11 @@ flag = FlagData()
 
 while tmr_plot.is_notfinished():
     if tmr_plot.do_run():
-        tick = tmr_plot.tick
-
         if flag.flag:
-            apriltag_publisher(0.01*tick, 0.01*tick, 0.01*tick)
-            # rpy_publisher(0.02*tick, 0.02*tick, 0.02*tick)
-            print("STEP 2")
             sim_traj = SimulationData()
             sim_data = np.array(sim_traj.traj).reshape((sim_traj.length, sim_traj.height))
             print(sim_data.shape)
+
         else:
-            apriltag_publisher(0,0,0)
-            # rpy_publisher(0,0,0)
-            print("STEP 1")
+            print(tmr_plot.tick)
 
--- test_spc.py
+++ test_spc.py
@@ -1,38 +1,25 @@
 import rospy
 import numpy as np
-from ros_np_multiarray import ros_np_multiarray as rnm
 
+from ros_np_multiarray import ros_np_multiarray as rnm
 from classes.timer import Timer
 from publisher import flag_publisher, simulation_publisher
-from subscriber import ApriltagData, RPYData
 
 rospy.init_node('spc', anonymous=True)
 
 tmr_plot = Timer(_name='Plot',_HZ=1,_MAX_SEC=100,_VERBOSE=True)
 tmr_plot.start()
 
-test_traj = np.load("test_traj.npy")
-data = rnm.to_multiarray_f32(test_traj)
+test_traj = np.ones(shape=(502,2))
+traj_data = rnm.to_multiarray_f32(test_traj)
 print("Initialization complete")
-
-tick = 0
 
 while tmr_plot.is_notfinished():
     if tmr_plot.do_run():
-        if tick < 5:
-            print("STEP 1")
+        if tmr_plot.tick < 5:
             flag_publisher(0)
-            tick += 1
 
         else: 
-            print("STEP 2")
             flag_publisher(1)
-            simulation_publisher(data)
-            apriltag = ApriltagData()
-            # rpy = RPYData()
-
-            apriltag_current = np.array([[apriltag.x, apriltag.y, apriltag.yaw]])
-            print("apriltag data: ", apriltag_current)
-            
-            # rpy_current = np.array([[rpy.r_data, rpy.p_data, rpy.y_data]])
-            # print("rpy data: ", rpy_current)
+            simulation_publisher(traj_data)
+            print(tmr_plot.tick)
